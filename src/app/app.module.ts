06209992a6a5be53b7fa3451cc0e4fd55ce782a9
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-05-19 22:03:32 -0400

    Fixed app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
-import { InputGuessComponent } from './components/input-guess/input-guess.component';
 import { RadioButtonComponent } from './components/radio-button/radio-button.component';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { RadioButtonComponent } from './components/radio-button/radio-button.com
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    InputGuessComponent,
     RadioButtonComponent
   ],
   imports: [
