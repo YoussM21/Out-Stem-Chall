ab12d0d9639b6a025d77432468ad3c57884d5a71
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-05-19 21:55:14 -0400

    Create radio button

---
+++ src/app/components/radio-button/radio-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-button',
+  templateUrl: './radio-button.component.html',
+  styleUrls: ['./radio-button.component.css']
+})
+export class RadioButtonComponent {
+}
